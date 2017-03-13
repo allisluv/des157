7739577f3fd96737933ef83207990a964c24b68d
allisluv <allisonlove@campus-009-012.ucdavis.edu>
2017-03-13 12:23:54 -0700

    sketch page fix

---
+++ project/proto3/header.js
@@ -0,0 +1,41 @@
+// JavaScript Document
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM fully loaded and parsed");
+
+    // all other js here
+    var here = document.getElementById('here');
+    var text = here.style.color.value;
+    var back = here.style.backgroundColor.value;
+    here.style.color = back;
+    here.style.backgrounColor = text;
+
+    var menu = document.getElementById('menu');
+    var arrow = document.getElementById('arrowSide');
+    var list = document.getElementsByClassName('nav');
+    var start = 20;
+    arrow.style.webkitTransform = "rotate(0deg)";
+
+    menu.addEventListener("click", open_close)
+
+  /*  arrow.addEventListener("click", open_close) */
+
+    function open_close() {
+      if (arrow.style.webkitTransform == "rotate(0deg)") {
+        arrow.style.webkitTransform = "rotate(180deg)";
+        var spread = start;
+        for (var i=0; i <= list.length; i++) {
+          list[i].style.left = spread+"px";
+          spread = spread + 145;
+        }
+      } else {
+        arrow.style.webkitTransform = "rotate(0deg)";
+        for (var i=0; i <= list.length; i++) {
+          list[i].style.left = "-165px";
+        }
+      }
+      list[i].style.left = "-165px";
+      console.log(spread);
+    }
+
+});
