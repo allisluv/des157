3e8d23833ea9f6b093119614d7bd195f27cebb07
allisluv <allisonlove@Allisons-MacBook-Pro.local>
2017-03-14 12:15:40 -0700

    added images

--- project/proto3/userinfo.js
+++ project/proto3/userinfo.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
     // all other js here
     //for the sign in page
-alert ("Thank you for testing the Art of Collaboration Prototype. To sign in, type 'test' into the user name and password feilds. Remember that the prototype doesn't save work yet, so your work will clear when you leave the site.")
+alert ("Thank you for testing the Art of Collaboration Prototype 3. To sign in, type 'test' into the user name and password feilds. Remember that the prototype doesn't save work yet, so your work will clear when you leave a page.")
     var prompt = document.getElementById("prompt");
 
     document.signIn.onsubmit = function () {
